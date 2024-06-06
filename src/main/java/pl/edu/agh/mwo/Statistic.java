ae364b8b26a3b581f015686a308538a699ddc72a
Piotr <pjachera@gmail.com>
2024-06-06 20:38:25 +0200

    cleanup class, change names and completing the function

--- src/main/java/pl/edu/agh/mwo/Statistic.java
+++ src/main/java/pl/edu/agh/mwo/Statistic.java
@@ -1,10 +1,20 @@
 package pl.edu.agh.mwo;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Statistic {
     private List<PitStop> pitstops = new ArrayList<>();
+
+
+    public Statistic(){}
 
     public Statistic(List<PitStop> pitstops) {
         this.pitstops = pitstops;
@@ -13,7 +23,7 @@ public class Statistic {
     public PitStop getMinPitDuration(){
         PitStop min = new PitStop();
         for(PitStop pitStop : pitstops){
-            if(min == null) min = pitStop;
+            if(min.getSpit_duration() == 0) min = pitStop;
             if(pitStop.getSpit_duration() < min.getSpit_duration()){
                 min = pitStop;
             }
@@ -21,16 +31,16 @@ public class Statistic {
         return min;
     }
 
-    public int getBestDurationFromDriverNumber(int driver){
-        int duration=0;
-        int min=0;
-//        for(PitStop pitStop : pitstops){
-//            if(pitStop.getSdriver_number() == driver){
-////                 if(< min.getSpit_duration()){
-////
-////                }
-//            }
-//        }
+    public PitStop getMinPitDurationFromDriverNumber(int driver){
+        PitStop min = new PitStop();
+        for(PitStop pitStop : pitstops){
+            if(pitStop.getSdriver_number() == driver) {
+                if (min.getSpit_duration() == 0) min = pitStop;
+                if (pitStop.getSpit_duration() < min.getSpit_duration()) {
+                    min = pitStop;
+                }
+            }
+        }
         return min;
     }
 
