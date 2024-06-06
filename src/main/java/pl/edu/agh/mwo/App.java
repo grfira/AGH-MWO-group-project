64f491e4b205ad908aadc056b4d38b452505a63b
Piotr <pjachera@gmail.com>
2024-06-07 00:14:26 +0200

    Changing LocalDateTime to String ant ints to longs because i have issues with compilation

--- src/main/java/pl/edu/agh/mwo/App.java
+++ src/main/java/pl/edu/agh/mwo/App.java
@@ -2,7 +2,9 @@ package pl.edu.agh.mwo;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -13,24 +15,23 @@ import java.util.List;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException, ParseException {
         System.out.println( "Hello World!" );
 
-//        List<PitStop> pitStopList = new ArrayList<>();
-//
-//        JSONArray jsonArray = PitStopReader.readJsonFromFile("pitstops-data.json");
-//        Iterator<JSONObject> iterator = jsonArray.iterator();
-//
-//        while (iterator.hasNext()) {
-//            pitStopList.add(new PitStop(iterator.next()));
-//        }
-//
-//        Statistic statistic = new Statistic(pitStopList);
-//
-//        System.out.println(statistic.getMinPitDuration());
-//
-//       // System.out.println(pitStopList.get(5).getSession_key());
+        List<PitStop> pitStopList = new ArrayList<>();
+
+        JSONArray jsonArray = PitStopReader.readJsonFromFile("pitstops-data.json");
+        Iterator<JSONObject> iterator = jsonArray.iterator();
+
+        while (iterator.hasNext()) {
+            pitStopList.add(new PitStop(iterator.next()));
+        }
+
+        Statistic statistic = new Statistic(pitStopList);
+
+        System.out.println("Najnizszy pit duration miał kierowca o numerze: " + statistic.getMinPitDuration().getSdriver_number());
+
+       // System.out.println(pitStopList.get(5).getSession_key());
 
 
     }
--- src/main/java/pl/edu/agh/mwo/PitStop.java
+++ src/main/java/pl/edu/agh/mwo/PitStop.java
@@ -7,22 +7,22 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 
 public class PitStop {
-    private int session_key;
-    private int smeeting_key;
-    private LocalDateTime sdate;
-    private int sdriver_number;
+    private long session_key;
+    private long smeeting_key;
+    private String  sdate;
+    private long sdriver_number;
     private double spit_duration;
-    private int slap_number;
+    private long slap_number;
 
     public PitStop() {
 
     }
 
-    public int getSession_key() {
+    public long getSession_key() {
         return session_key;
     }
 
-    public PitStop(int session_key, int smeeting_key, LocalDateTime  sdate, int sdriver_number, double spit_duration, int slap_number) {
+    public PitStop(long session_key, long smeeting_key, String   sdate, long sdriver_number, double spit_duration, long slap_number) {
         this.session_key = session_key;
         this.smeeting_key = smeeting_key;
         this.sdate = sdate;
@@ -34,40 +34,40 @@ public class PitStop {
     public PitStop(JSONObject jsonObject) throws IOException, ParseException {
 
 
-        this.session_key = (int) jsonObject.get("session_key");
-        this.smeeting_key = (int) jsonObject.get("meeting_key");
-        this.sdate = (LocalDateTime) jsonObject.get("date");
-        this.sdriver_number = (int) jsonObject.get("driver_number");
+        this.session_key = (long) jsonObject.get("session_key");
+        this.smeeting_key = (long) jsonObject.get("meeting_key");
+        this.sdate = (String ) jsonObject.get("date");
+        this.sdriver_number = (long) jsonObject.get("driver_number");
         this.spit_duration = (double) jsonObject.get("pit_duration");
-        this.slap_number = (int) jsonObject.get("lap_number");
+        this.slap_number = (long) jsonObject.get("lap_number");
 
     }
 
-    public void setSession_key(int session_key) {
+    public void setSession_key(long session_key) {
         this.session_key = session_key;
     }
 
-    public int getSmeeting_key() {
+    public long getSmeeting_key() {
         return smeeting_key;
     }
 
-    public void setSmeeting_key(int smeeting_key) {
+    public void setSmeeting_key(long smeeting_key) {
         this.smeeting_key = smeeting_key;
     }
 
-    public LocalDateTime  getSdate() {
+    public String   getSdate() {
         return sdate;
     }
 
-    public void setSdate(LocalDateTime sdate) {
+    public void setSdate(String sdate) {
         this.sdate = sdate;
     }
 
-    public int getSdriver_number() {
+    public long getSdriver_number() {
         return sdriver_number;
     }
 
-    public void setSdriver_number(int sdriver_number) {
+    public void setSdriver_number(long sdriver_number) {
         this.sdriver_number = sdriver_number;
     }
 
@@ -75,15 +75,15 @@ public class PitStop {
         return spit_duration;
     }
 
-    public void setSpit_duration(int spit_duration) {
+    public void setSpit_duration(double spit_duration) {
         this.spit_duration = spit_duration;
     }
 
-    public int getSlap_number() {
+    public long getSlap_number() {
         return slap_number;
     }
 
-    public void setSlap_number(int slap_number) {
+    public void setSlap_number(long slap_number) {
         this.slap_number = slap_number;
     }
 }
